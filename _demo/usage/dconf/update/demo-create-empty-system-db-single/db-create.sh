514a85e1adb6da2256ab94b1dbf0a8248f12c1eb
samwhelp <samwhelp@users.noreply.github.com>
2023-01-09 21:39:32 +0800

    fix

--- _demo/usage/dconf/update/demo-create-empty-system-db-single/db-create.sh
+++ _demo/usage/dconf/update/demo-create-empty-system-db-single/db-create.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 
-sudo install -Dm644 ./asset/overlay/etc/dconf/profile/user /etc/dconf/profile/user
+#sudo install -Dm644 ./asset/overlay/etc/dconf/profile/user /etc/dconf/profile/user
 
 sudo mkdir -p /etc/dconf/db/site.d
 
---
+++ _demo/usage/dconf/update/demo-create-empty-system-db-single/profile-install.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+
+sudo install -Dm644 ./asset/overlay/etc/dconf/profile/user /etc/dconf/profile/user
